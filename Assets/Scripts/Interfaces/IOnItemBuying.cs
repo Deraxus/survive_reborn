a9b7bbd42fbabcd5713c9d2bbfdf0aba78acd613
Deraxus <deraxus32@gmail.com>
2026-04-25 00:22:04 +0300

    123

--- Assets/Scripts/Interfaces/IOnItemBuying.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IOnItemBuying
-{
-    // Start is called before the first frame update
-    void OnItemBuying(GameObject item, ItemManager itemManager);
-}
